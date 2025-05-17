9aa04950edab69c2f0108335c2e2ba9ff0fce17f
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-05-17 14:44:33 +0200

    fixing bug in flashcard process

--- src/store/cardProcess.ts
+++ src/store/cardProcess.ts
@@ -6,15 +6,15 @@ interface State extends cardProcessInfo {
     getAnswer: (limit: number) => Promise<void>
 }
 
-export const useCardAnswerStore = create<State>((set) => {
-    return {
-        getAnswer: async (limit: number) => {
-            const datos = await getMockData()
-            const { data } = datos
-            const answers = data.map((answer) => answer.respuesta)
-            set({ answer: answers })
+export const useCardAnswerStore = create<State>((set) => ({
+    answers: [],
 
-        }
+    getAnswer: async (limit: number) => {
+        const datos = await getMockData()
+        const { data } = datos
+        const answers = data.map((answer) => answer.respuesta)
+        set({ answers: answers })
+
     }
-}
-)
+
+}))
