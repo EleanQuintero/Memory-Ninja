72e0d90bf7543bd5aceb4852758a6322abdb5dd5
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-07-30 01:47:23 +0200

    server endpoint count flashcard by theme

---
+++ src/app/api/dashboard/count-flashcards-by-theme/route.ts
@@ -0,0 +1,31 @@
+import { rateLimitter } from "@/middleware/rate-limit";
+import { NextResponse } from "next/server";
+import { currentUser } from "@clerk/nextjs/server";
+
+async function getCountFlashcardsByTheme() {
+    try {
+        const user = await currentUser()
+        const userId = user?.id
+        console.log(userId)
+
+        const response = await fetch(`http://localhost:4444/api/dashboard/countByTheme/${userId}`)
+        if (!response.ok) {
+            throw new Error("Error al realizar la peticion")
+        }
+
+        const data = await response.json()
+        console.log(data)
+
+        return NextResponse.json(data)
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: "Error al solicitar las estadisticas" },
+            { status: 500 }
+        );
+    }
+
+}
+
+export const GET = rateLimitter({ fn: getCountFlashcardsByTheme });
