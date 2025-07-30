087c8e0fc58a97e95e1d2c24f4f0062492244c5d
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-07-30 02:49:12 +0200

    mover la inicialización de QueryClient dentro de la función DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,9 +4,9 @@ import { Protect } from "@clerk/nextjs";
 import Dashboard from "./components/Dashboard";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const dashboardClient = new QueryClient();
-
 export default function DashboardPage() {
+  const dashboardClient = new QueryClient();
+
   return (
     <Protect plan={"pro_user"} fallback={<SubscriptionFallback />}>
       <QueryClientProvider client={dashboardClient}>
