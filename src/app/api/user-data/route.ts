2a61b949c6eb268ebfbbd042e438e7a9f793698c
EleanQuintero <elean.quintero.madrid@gmail.com>
2025-11-15 00:39:10 +0100

    feat: agregar soporte para el webhook de actualización de usuario y mejorar el proceso de onboarding

--- src/app/api/user-data/route.ts
+++ src/app/api/user-data/route.ts
@@ -29,10 +29,9 @@ export async function POST(req: NextRequest) {
         // Validar datos del evento
         const newUser = {
             id: evt.data.id,
-            name: evt.data.first_name,
-            lastName: evt.data.last_name,
             email: evt.data.email_addresses?.[0]?.email_address,
-            role: 1
+            role: 1,
+            username: evt.data.username,
         }
 
         if (newUser.id === undefined || newUser.email === undefined) {
